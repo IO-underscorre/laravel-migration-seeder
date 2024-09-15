2a39dcc616ffe63e2f27236a15e64232e31228e4
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-09-15 11:37:54 +0200

    feat: controller created and routes adjusted to display trains data on homepage

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,15 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    $title = 'Welcome!';
-
-    $main_sections = [
-        [
-            'section_title' => 'Section',
-            'section_text' => 'Code ipsum variable loop sit amet, null pointer exception semicolon error integer. Debugging stack overflow binary, function void recursive syntax spaghetti. Console log array overflow bracket callback return undefined. Null object keyframe refactor snippet pseudocode regex div flexbox float overflow hidden. Semicolon compile iterate breakpoint, inline const bug bash. Merge conflict for loop await async, deploy fetch API.',
-        ]
-    ];
-
-    return view('home', compact('title', 'main_sections'));
-})->name('home');
+Route::get('/', [PageController::class, 'trains'])->name('home');
